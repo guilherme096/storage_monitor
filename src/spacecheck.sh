184f41586f651c818e4fc2bd07c713facf464a61
guilherme096 <guilherme.rosa60@gmail.com>
2023-10-29 11:40:15 +0000

    added size and changed to only print dirs

--- src/spacecheck.sh
+++ src/spacecheck.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+# variables
 dir=$(pwd)
 
-items=$(find $dir)
+# find only dirs and subdirs
+items=$(find $dir -type d)
 
+# gets the size of one item
+get_size(){
+    item=$1
+    size=$(du -s -b $item | awk '{print $1}')
+}
+
+# lists items in the items variable with their size
 list_items(){
-    for item in $items
-    do
-        echo $item 
+    for item in $items; do
+        size=''
+        get_size $item
+        echo -e "$size\t$item" 
     done
 }
 
