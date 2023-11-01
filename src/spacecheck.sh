afb0a7df821ddbca6a29a48e70d32bfcda3a46bd
guilherme096 <guilherme.rosa60@gmail.com>
2023-11-01 15:53:50 +0000

    chechk each flag

--- src/spacecheck.sh
+++ src/spacecheck.sh
@@ -6,10 +6,27 @@ dir="./samples/"
 # find only dirs and subdirs
 items=$(find $dir -type d)
 
-valid_flags="-n -d -s -r -a -l"
-flags=""
+
+n_flag=0
+d_flag=0
+s_flag=0
+r_flag=0
+l_flag=0
 
 regex=""
+
+# get flags
+while getopts 'ndslr' opt; do
+    case $opt in
+        n) n_flag=1;;
+        d) d_flag=1;;
+        s) s_flag=1;;
+        l) l_flag=1;;
+        r) r_flag=1; regex=$OPTARG;;
+        \?) echo "Invalid option: -$OPTARG" >&2;;
+    esac
+done
+
 
 
 # gets the size of one item
