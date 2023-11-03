162f22d123b54b57cdf50bd11d76305a2fd458e9
guilherme096 <guilherme.rosa60@gmail.com>
2023-11-03 15:51:59 +0000

    add testing with bats

--- executer.sh
+++ executer.sh
@@ -4,7 +4,7 @@ docker-compose up -d
 
 CONTAINER_ID=$(docker-compose ps -q fedora-app)
 
-docker exec -it "$CONTAINER_ID" bash -c "cd /app && bash"
+docker exec -it "$CONTAINER_ID" bash -c "dnf -y install bats && cd /app && bash"
  
 docker-compose stop
 
